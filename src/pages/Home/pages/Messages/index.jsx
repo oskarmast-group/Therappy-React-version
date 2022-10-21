509efe03479241049de8e4f51539dc68344c04ba
Cedric Borreguín <cborreguin@gmail.com>
2022-10-21 10:35:54 -0500

    [ADD] Messages screen

--- src/pages/Home/pages/Messages/index.jsx
+++ src/pages/Home/pages/Messages/index.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { PRIMARY_GREEN } from 'resources/constants/colors';
+import useUser from 'state/user';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    margin: 0;
+    font-size: 30px;
+    font-weight: 600;
+    color: ${PRIMARY_GREEN};
+    text-align: center;
+`;
+
+const Notice = styled.span`
+    margin: 0;
+    margin-top: 20px;
+    text-align: center;
+`;
 
 const Messages = () => {
-  return (<div>Mensajes</div>);
-}
+  const [user] = useUser();
+  const messageList = [];
+    return (
+        <>
+            <Title>Mensajes</Title>
+            {messageList.length === 0 ? <Notice>{user.user?.userType === 'therapist' ? "Cuando te pongas en contacto con algún cliente tus mensajes aparecerán aquí" : "Cuando te pongas en contacto con algún especialista tus mensajes aparecerán aquí"}</Notice> : <div></div>}
+        </>
+    );
+};
 
 export default Messages;
